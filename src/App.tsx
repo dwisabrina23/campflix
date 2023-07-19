151d4a2f05d66d0a755fb71e06ae26eb9be94113
Dwi Sabrina <dwisabrina10@gmail.com>
2023-07-19 17:32:59 +0800

    style: fix import to more consistent

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Home from "@/pages/Home";
-import SearchPage from "./pages/Search";
+import SearchPage from "@/pages/Search";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
